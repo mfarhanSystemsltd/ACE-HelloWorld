af78ff323a158f4e20a99d88beddd6bbcbe77eec
mfarhanSystemsltd <88446631+mfarhanSystemsltd@users.noreply.github.com>
2022-01-21 00:20:34 +0500

    Update helm.sh

--- helm.sh
+++ helm.sh
@@ -1,5 +1,5 @@
 helm version
-helm registry login --username AWS --password $(aws ecr get-login-password --region us-east-2) 736505676141.dkr.ecr.us-east-2.amazonaws.com'
+helm registry login --username AWS --password $(aws ecr get-login-password --region us-east-2) 736505676141.dkr.ecr.us-east-2.amazonaws.com
 export HELM_EXPERIMENTAL_OCI=1
 helm pull oci://736505676141.dkr.ecr.us-east-2.amazonaws.com/ace-deployment --version 0.1.0
 tar -zxvf ace-deployment-0.1.0.tgz
